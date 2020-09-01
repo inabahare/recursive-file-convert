9a849e420f77e85d855160d6447878f40c8e1b13
inaba <software@panduro.guru>
2020-09-01 02:42:54 +0200

    Added functions to handle spawning commands

--- ffmpeg/FfmpegMethods.cs
+++ ffmpeg/FfmpegMethods.cs
@@ -1,41 +1,58 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Terminal;
 
 namespace VideoConvert
 {
   public partial class Ffmpeg : IFfmpeg
   {
+    Regex nbFrames = new Regex(@"(?<=(nb_frames\=))\d+");
+
     string FfmpegParams
     {
       get => $"{_codec} {_crf} {_preset} {_fastStart}";
     }
 
+    int GetFrameCount(string videoPath)
+    {
+      return 0;
+    }
+
     public void Convert(string input, string output)
     {
-      var process = new Process
+      var frameCount = GetFrameCount(input);
+      var command = new Command
       {
-        StartInfo = {
-          FileName = "ffmpeg",
-          Arguments = $"-i \"{input}\" {FfmpegParams} \"{output}\"",
-          UseShellExecute = false,
-          RedirectStandardOutput = true,
-          RedirectStandardError = true
-        }
+        OnStdout = data => { Console.WriteLine($"StdOut {data}"); },
+        OnStdErr = data => { Console.WriteLine($"StdErr {data}"); }
       };
 
-      process.Start();
+      command.Run("ffmpeg", $"-i \"{input}\" {FfmpegParams} \"{output}\"");
+      // var process = new Process
+      // {
+      //   StartInfo = {
+      //     FileName = "ffmpeg",
+      //     Arguments = $"-i \"{input}\" {FfmpegParams} \"{output}\"",
+      //     UseShellExecute = false,
+      //     RedirectStandardOutput = true,
+      //     RedirectStandardError = true
+      //   }
+      // };
 
-      process.ErrorDataReceived +=
-        (sender, line) =>
-        {
-          if (line.Data != null)
-            Console.WriteLine(line.Data);
-        };
+      // process.Start();
+
+      // process.ErrorDataReceived +=
+      //   (sender, line) =>
+      //   {
+      //     if (line.Data != null)
+      //       Console.WriteLine(line.Data);
+      //   };
 
 
-      process.BeginErrorReadLine();
-      process.WaitForExit();
+      // process.BeginErrorReadLine();
+      // process.WaitForExit();
     }
   }
 }
