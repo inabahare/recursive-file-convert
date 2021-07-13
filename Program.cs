049fe4fdac48012d375875b5830633f44271bcce
inaba <software@panduro.guru>
2021-07-13 22:30:51 +0200

    Converting

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using FFMpegCore;
+using FFMpegCore.Enums;
 
 namespace recursive_file_convert
 {
@@ -6,6 +9,28 @@ namespace recursive_file_convert
     {
         static void Main(string[] args)
         {
+            var path = "/home/inaba/Videos/TV Shorts";
+            
+            var files = new DirectoryInfo(path).GetFiles();
+
+            foreach (var file in files) {
+                var name = Path.GetFileNameWithoutExtension(file.Name);
+                var directoryName = file.DirectoryName;
+                var extension = file.Extension;
+
+                var tmpName = $"{directoryName}/{name}.temp{extension}";
+
+                FFMpegArguments
+                    .FromFileInput(file.FullName)
+                    .OutputToFile(tmpName, false, options => options
+                        .WithVideoCodec("libx265")
+                        .WithConstantRateFactor(28)
+                        .WithFastStart()
+                    )
+                    .NotifyOnProgress(onTimeProgress => Console.WriteLine(onTimeProgress))
+                    .ProcessSynchronously();
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
