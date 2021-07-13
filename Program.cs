a91e004561374cdb18c2f37c65edc5780c55240f
inaba <software@panduro.guru>
2021-07-13 21:19:14 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace recursive_file_convert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
