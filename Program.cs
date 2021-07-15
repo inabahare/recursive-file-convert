92a46a30f817f37fa042b98e9d4079144c096620
inaba <software@panduro.guru>
2021-07-15 14:59:41 +0200

    Listing already converted

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using FFMpegCore;
@@ -10,10 +12,11 @@ namespace recursive_file_convert
     {
         static async Task Main(string[] args)
         {
+            var converted = new List<string>();
             var path = "/home/inaba/Videos/TV Shorts";
-            
             var files = new DirectoryInfo(path).GetFiles();
-
+            
+            
             foreach (var file in files) {
                 var name = Path.GetFileNameWithoutExtension(file.Name);
                 var directoryName = file.DirectoryName;
@@ -21,14 +24,28 @@ namespace recursive_file_convert
 
                 var tmpName = $"{directoryName}/{name}.temp{extension}";
 
+                // TODO: Add ffprobe and get percentages
+
                 await Ffmpeg.Convert(
                     file.FullName, 
                     tmpName,
                     progress => {
                         Console.Clear();
-                        Console.WriteLine(progress);
+                        var alreadyConverted = "";
+                        for (var i = 0; i < converted.Count; i++) {
+                            alreadyConverted += $"{i + 1} - {converted[i]}\n";
+                        }
+
+                        var converting = $"{file.FullName} - {progress}";
+
+                        var result = 
+                            $"Converted:\n{alreadyConverted}\n\nConverting:\n{converting}";
+                        
+                        Console.WriteLine(result);
                     }
                 );
+
+                converted.Add(file.FullName);
             }
 
             Console.WriteLine("Hello World!");
