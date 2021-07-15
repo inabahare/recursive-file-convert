e966b51087fb35a062118905785e11dd5f284f3b
inaba <software@panduro.guru>
2021-07-15 15:08:37 +0200

    Fix formatting

--- Ffmpeg.cs
+++ Ffmpeg.cs
@@ -6,21 +6,21 @@ using FFMpegCore.Enums;
 
 namespace recursive_file_convert
 {
-    class Ffmpeg
+  class Ffmpeg
+  {
+    public static async Task Convert(string input,
+                                        string output,
+                                        Action<TimeSpan> onProgress)
     {
-        public static async Task Convert(string input, 
-                                         string output,
-                                         Action<TimeSpan> onProgress)
-        {
-            await FFMpegArguments
-                .FromFileInput(input)
-                .OutputToFile(output, true, options => options
-                    .WithVideoCodec("libx265")
-                    .WithConstantRateFactor(28)
-                    .WithFastStart()
-                )
-                .NotifyOnProgress(onProgress)
-                .ProcessAsynchronously();
-        }
+      await FFMpegArguments
+          .FromFileInput(input)
+          .OutputToFile(output, true, options => options
+              .WithVideoCodec("libx265")
+              .WithConstantRateFactor(28)
+              .WithFastStart()
+          )
+          .NotifyOnProgress(onProgress)
+          .ProcessAsynchronously();
     }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -8,49 +8,48 @@ using FFMpegCore.Enums;
 
 namespace recursive_file_convert
 {
-    class Program
+  class Program
+  {
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
-        {
-            var converted = new List<string>();
-            var path = "/home/inaba/Videos/TV Shorts";
-            var files = new DirectoryInfo(path).GetFiles();
-            
-            
-            foreach (var file in files) {
-                var name = Path.GetFileNameWithoutExtension(file.Name);
-                var directoryName = file.DirectoryName;
-                var extension = file.Extension;
-
-                var tmpName = $"{directoryName}/{name}.temp{extension}";
-
-                // TODO: Add ffprobe and get percentages
-
-                await Ffmpeg.Convert(
-                    file.FullName, 
-                    tmpName,
-                    progress => {
-                        Console.Clear();
-                        var alreadyConverted = "";
-                        for (var i = 0; i < converted.Count; i++) {
-                            alreadyConverted += $"{i + 1} - {converted[i]}\n";
-                        }
-
-                        var converting = $"{file.FullName} - {progress}";
-
-                        var result = 
-                            $"Converted:\n{alreadyConverted}\n\nConverting:\n{converting}";
-                        
-                        Console.WriteLine(result);
-                    }
-                );
-
-                converted.Add(file.FullName);
-            }
-
-            Console.WriteLine("Hello World!");
-        }
+      var converted = new List<string>();
+      var path = "/home/inaba/Videos/TV Shorts";
+      var files = new DirectoryInfo(path).GetFiles();
 
 
+      foreach (var file in files)
+      {
+        var name = Path.GetFileNameWithoutExtension(file.Name);
+        var directoryName = file.DirectoryName;
+        var extension = file.Extension;
+
+        var tmpName = $"{directoryName}/{name}.temp{extension}";
+
+        // TODO: Add ffprobe and get percentages
+
+        await Ffmpeg.Convert(
+          file.FullName,
+          tmpName,
+          progress =>
+          {
+            Console.Clear();
+            var alreadyConverted = "";
+            for (var i = 0; i < converted.Count; i++)
+              alreadyConverted += $"{i + 1} - {converted[i]}\n";
+
+            var converting = $"{file.FullName} - {progress}";
+
+            var result =
+                        $"Converted:\n{alreadyConverted}\n\nConverting:\n{converting}";
+
+            Console.WriteLine(result);
+          }
+        );
+
+        converted.Add(file.FullName);
+      }
+
+      Console.WriteLine("Hello World!");
     }
+  }
 }
